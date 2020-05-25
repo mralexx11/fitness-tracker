558e5b392d656be8854ae25d085fb68b15fcc3f2
Alex <42402960+mralexx11@users.noreply.github.com>
2020-05-25 21:42:28 +0300

    navigation

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 
 // @ts-ignore
@@ -22,7 +23,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   exports: [
     MatButtonModule,
@@ -33,7 +35,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
+
   ]
 })
 export class MaterialModule {}
