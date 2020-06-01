bb0472b22511f88c00ceee68be8316abe1846bb4
Alex <42402960+mralexx11@users.noreply.github.com>
2020-06-01 23:13:44 +0300

    progress stop + button

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -7,12 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
+  timer: number;
+
   constructor() { }
 
   ngOnInit(): void {
-    setInterval(() => {
-      this.progress < 100 ? this.progress = this.progress + 5 : this.progress = 100;
+    this.timer = setInterval(() => {
+      this.progress >= 100 ? clearInterval(this.timer) : this.progress = this.progress + 20;
     }, 1000);
   }
 
+  onStop() {
+    clearInterval(this.timer)
+  }
 }
